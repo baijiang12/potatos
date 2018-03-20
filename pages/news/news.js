4303b147ffb4e8b6e19a7bbfe371485488e21c30
starstarman <83170989@qq.com>
2018-03-20 09:09:27 +0800

    potato-0.2.0合并咨询首页

--- pages/news/news.js
+++ pages/news/news.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    imgUrls: [
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521473355301&di=6dc5dceaae35bb9485a586a0e2751711&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01f402565e5b5032f875ae3401ed18.png',
+      'http://image.wufazhuce.com/Fv7sIjdPR0CceEDIZ4wd62jWAhTU',
+      'http://image.wufazhuce.com/FstbIkUvTiKbCRpGoeqvk_fQKgio',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522067917&di=9b5ee7480672766eddd3fb56bf7fc270&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01b221565e5b516ac7255d2e98bbcb.jpg%401280w_1l_2o_100sh.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521473246831&di=fb8ed098328bb4b928321e2999cbc74e&imgtype=0&src=http%3A%2F%2Fpic.58pic.com%2F58pic%2F13%2F78%2F80%2F95c58PICn2f_1024.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521474064312&di=a8ed7acf04007c4675e46219d943aa10&imgtype=0&src=http%3A%2F%2Feasyread.ph.126.net%2FpGkEQU81Eyc0_7NJk_Z1pQ%3D%3D%2F7916704721824964137.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
 
   /**
