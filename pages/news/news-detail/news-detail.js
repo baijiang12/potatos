f9845bdddc9f95de754e3a4ad76a658bd9619121
starstarman <83170989@qq.com>
2018-03-22 16:38:01 +0800

    potato-0.2.5评论完成

--- pages/news/news-detail/news-detail.js
+++ pages/news/news-detail/news-detail.js
@@ -5,6 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    info:'',
+    flag: false  ,
+    showModalStatus: false,
     height: 20,
     focus: false,
     showModal: false,
@@ -18,7 +21,65 @@ Page({
       'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1521474064312&di=a8ed7acf04007c4675e46219d943aa10&imgtype=0&src=http%3A%2F%2Feasyread.ph.126.net%2FpGkEQU81Eyc0_7NJk_Z1pQ%3D%3D%2F7916704721824964137.jpg'
     ],
   },
+  bindFormSubmit: function (e) {
+    var textareaContent = e.detail.value.textarea;
+    textareaContent = '';
+    this.setData({
+      info:''
+    })
+  },
+  powerDrawer: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    });
 
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation;
+
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step();
+
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭  
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示  
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -54,9 +115,9 @@ Page({
   bindTextAreaBlur: function (e) {
     console.log(e.detail.value)
   },
-  bindFormSubmit: function (e) {
-    console.log(e.detail.value.textarea)
-  },
+  // bindFormSubmit: function (e) {
+  //   console.log(e.detail.value.textarea)
+  // },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
