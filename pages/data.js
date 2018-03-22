92a283c1f5507a9023aee4baf0eb829bb966c7f1
starstarman <83170989@qq.com>
2018-03-22 21:58:03 +0800

    potato-0.2.7 本地数据

---
+++ pages/data.js
@@ -0,0 +1,48 @@
+var local_database = [
+  {
+    matchId : 1,
+    title : '足球',
+    img : "/images/sports/football1.png",
+    status : 1
+  },
+  {
+    matchId : 2,
+    title : '篮球',
+    img: "/images/sports/basketball.png",
+    status : 0
+  },
+  {
+    matchId : 3,
+    title : '排球',
+    img: "/images/sports/volleyball.png",
+    status : 0
+  },
+    {
+    matchId : 4,
+    title : '乒乓球',
+    img: "/images/sports/pingpongball.png",
+    status : 0
+  },
+    {
+      matchId: 5,
+      title: '游泳',
+      img: "/images/sports/swimming.png",
+      status: 0
+    },
+    {
+      matchId: 6,
+      title: '网球',
+      img: "/images/sports/tennisball.png",
+      status: 0
+    },
+    {
+      matchId: 7,
+      title: '散打',
+      img: "/images/sports/freeCombat.png",
+      status: 0
+    },
+]
+
+module.exports = {
+  match_list: local_database
+}
