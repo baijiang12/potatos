63127392ea776e63c0ce8a2ae02e27c835f42387
baijiang12 <2296772442@qq.com>
2018-04-02 18:14:56 +0800

    用户信息

--- utils/util.js
+++ utils/util.js
@@ -1,68 +1,101 @@
 function getDateDiff(dateTimeStamp) {
-    var result;
-    var minute = 1000 * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    var halfamonth = day * 15;
-    var month = day * 30;
-    var now = new Date().getTime();
-    var diffValue = now - dateTimeStamp;
-    if (diffValue < 0) {
-        return;
+  var result;
+  var minute = 1000 * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
+  var halfamonth = day * 15;
+  var month = day * 30;
+  var now = new Date().getTime();
+  var diffValue = now - dateTimeStamp;
+  if (diffValue < 0) {
+    return;
+  }
+  var monthC = diffValue / month;
+  var weekC = diffValue / (7 * day);
+  var dayC = diffValue / day;
+  var hourC = diffValue / hour;
+  var minC = diffValue / minute;
+  if (monthC >= 1) {
+    if (monthC <= 12)
+      result = "" + parseInt(monthC) + "月前";
+    else {
+      result = "" + parseInt(monthC / 12) + "年前";
     }
-    var monthC = diffValue / month;
-    var weekC = diffValue / (7 * day);
-    var dayC = diffValue / day;
-    var hourC = diffValue / hour;
-    var minC = diffValue / minute;
-    if (monthC >= 1) {
-        if (monthC <= 12)
-            result = "" + parseInt(monthC) + "月前";
-        else {
-            result = "" + parseInt(monthC / 12) + "年前";
-        }
-    }
-    else if (weekC >= 1) {
-        result = "" + parseInt(weekC) + "周前";
-    }
-    else if (dayC >= 1) {
-        result = "" + parseInt(dayC) + "天前";
-    }
-    else if (hourC >= 1) {
-        result = "" + parseInt(hourC) + "小时前";
-    }
-    else if (minC >= 1) {
-        result = "" + parseInt(minC) + "分钟前";
-    } else {
-        result = "刚刚";
-    }
+  }
+  else if (weekC >= 1) {
+    result = "" + parseInt(weekC) + "周前";
+  }
+  else if (dayC >= 1) {
+    result = "" + parseInt(dayC) + "天前";
+  }
+  else if (hourC >= 1) {
+    result = "" + parseInt(hourC) + "小时前";
+  }
+  else if (minC >= 1) {
+    result = "" + parseInt(minC) + "分钟前";
+  } else {
+    result = "刚刚";
+  }
 
-    return result;
+  return result;
 };
 function formatNumber(n) {
-    n = n.toString()
-    return n[1] ? n : '0' + n
+  n = n.toString()
+  return n[1] ? n : '0' + n
 }
 function formatTime(number, format) {
 
-    var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
-    var returnArr = [];
+  var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+  var returnArr = [];
 
-    var date = new Date(number);
-    returnArr.push(date.getFullYear());
-    returnArr.push(formatNumber(date.getMonth() + 1));
-    returnArr.push(formatNumber(date.getDate()));
+  var date = new Date(number);
+  returnArr.push(date.getFullYear());
+  returnArr.push(formatNumber(date.getMonth() + 1));
+  returnArr.push(formatNumber(date.getDate()));
 
-    returnArr.push(formatNumber(date.getHours()));
-    returnArr.push(formatNumber(date.getMinutes()));
-    returnArr.push(formatNumber(date.getSeconds()));
+  returnArr.push(formatNumber(date.getHours()));
+  returnArr.push(formatNumber(date.getMinutes()));
+  returnArr.push(formatNumber(date.getSeconds()));
 
-    for (var i in returnArr) {
-        format = format.replace(formateArr[i], returnArr[i]);
+  for (var i in returnArr) {
+    format = format.replace(formateArr[i], returnArr[i]);
+  }
+  return format;
+}
+
+function needUserInfo() {
+  wx.getSetting({
+    success: res => {
+      if (res.authSetting['scope.userInfo']) {
+        // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        wx.getUserInfo({
+          success: function (res) {
+            // wx.request({
+            //   url: 'http://47.95.4.127:8080/HeiKeOnline/users/' + that.globalData.userid + '.do',
+            //   data: {
+            //     id: userid,
+            //     name: res.userInfo.nickName,
+            //     icon: res.userInfo.avatarUrl,
+            //   },
+            //   method: 'PUT',
+            //   header: {
+            //     "Content-type": "application/json"
+            //   },
+            //   success: function (res) {
+            //     console.log(res)
+            //   },
+            //   fail: function () {
+
+            //   }
+            // })
+            wx.setStorageSync(userInfos, res.userInfo);
+          }
+        })
+      }
     }
-    return format;
+  })
 }
 module.exports = {
-    getDateDiff: getDateDiff,
-    formatTime:formatTime
+  getDateDiff: getDateDiff,
+  formatTime: formatTime
 }
