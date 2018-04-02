e2053e0a4d94c7332b12ed705c96cbe3e78977da
Yuejiawei <y2219531345>
2018-04-02 21:10:34 +0800

    base.js

---
+++ utils/base.js
@@ -0,0 +1,32 @@
+import { Config } from '../utils/config.js';
+class Base {
+  constructor() {
+    this.baseRequestUrl = Config.restUrl;
+  }
+  request(params) {
+    var that = this;
+    var url = this.baseRequestUrl + params.url;
+    if (!params.type) {
+      params.type = 'GET';  
+    }
+    wx.request({
+      url: url,
+      data: params.data,
+      method: params.type,
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        params.sCallback && params.sCallback(res.data);
+      },
+      fail: function (err) {
+        console.log(err);
+      }
+    })
+  }
+  //获得元素上的绑定的值
+  getDataSet(event, key) {
+    return event.currentTarget.dataset[key];
+  }
+}
+export { Base }
